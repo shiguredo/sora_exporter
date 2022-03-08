fc2ac8e88b81a8bf8132b22831246f33822f0d6a
NAKAI Ryosuke <nakai@shiguredo.jp>
2022-03-08 14:51:43 +0900

    template から移植する

--- main.go
+++ main.go
@@ -1,118 +1,172 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"errors"
-	"flag"
 	"fmt"
+	stdlog "log"
 	"net/http"
 	"os"
-	"os/signal"
-	"runtime/debug"
-	"syscall"
-	"time"
+	"os/user"
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/shiguredo/sora_exporter/collector"
+
+	"github.com/prometheus/common/promlog"
+	"github.com/prometheus/common/promlog/flag"
+	"github.com/prometheus/common/version"
+
+	promcollectors "github.com/prometheus/client_golang/prometheus/collectors"
+
+	"github.com/prometheus/exporter-toolkit/web"
+
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	listenAddr   string
-	metricsPath  string
-	timeout      time.Duration
 	soraURL      string
 	printVersion bool
-
-	version string
-	commit  string
-	date    string
 )
 
-func init() {
-	flag.StringVar(&listenAddr, "listen-addr", ":9199", "Address to listen for telemetry.")
-	flag.StringVar(&metricsPath, "metrics-path", "/metrics", "Path under which to expose metrics.")
-	flag.DurationVar(&timeout, "timeout", 5*time.Second, "Timeout for scraping to sora.")
-	flag.StringVar(&soraURL, "sora-url", "http://127.0.0.1:3000", "URL for sora stats endpoint.")
-	flag.BoolVar(&printVersion, "version", false, "Print version.")
+type handler struct {
+	unfilteredHandler http.Handler
+	// exporterMetricsRegistry is a separate registry for the metrics about
+	// the exporter itself.
+	exporterMetricsRegistry *prometheus.Registry
+	includeExporterMetrics  bool
+	maxRequests             int
+	logger                  log.Logger
+}
+
+func newHandler(includeExporterMetrics bool, maxRequests int, logger log.Logger) *handler {
+	h := &handler{
+		exporterMetricsRegistry: prometheus.NewRegistry(),
+		includeExporterMetrics:  includeExporterMetrics,
+		maxRequests:             maxRequests,
+		logger:                  logger,
+	}
+	if h.includeExporterMetrics {
+		h.exporterMetricsRegistry.MustRegister(
+			promcollectors.NewProcessCollector(promcollectors.ProcessCollectorOpts{}),
+			promcollectors.NewGoCollector(),
+		)
+	}
+	if innerHandler, err := h.innerHandler(); err != nil {
+		panic(fmt.Sprintf("Couldn't create metrics handler: %s", err))
+	} else {
+		h.unfilteredHandler = innerHandler
+	}
+	return h
+}
+
+// ServeHTTP implements http.Handler.
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	filters := r.URL.Query()["collect[]"]
+	level.Debug(h.logger).Log("msg", "collect query:", "filters", filters)
+
+	if len(filters) == 0 {
+		// No filters, use the prepared unfiltered handler.
+		h.unfilteredHandler.ServeHTTP(w, r)
+		return
+	}
+	// To serve filtered metrics, we create a filtering handler on the fly.
+	filteredHandler, err := h.innerHandler(filters...)
+	if err != nil {
+		level.Warn(h.logger).Log("msg", "Couldn't create filtered metrics handler:", "err", err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf("Couldn't create filtered metrics handler: %s", err)))
+		return
+	}
+	filteredHandler.ServeHTTP(w, r)
+}
+
+func (h *handler) innerHandler(filters ...string) (http.Handler, error) {
+	r := prometheus.NewRegistry()
+	r.MustRegister(version.NewCollector("sora_exporter"))
+	handler := promhttp.HandlerFor(
+		prometheus.Gatherers{h.exporterMetricsRegistry, r},
+		promhttp.HandlerOpts{
+			ErrorLog:            stdlog.New(log.NewStdlibAdapter(level.Error(h.logger)), "", 0),
+			ErrorHandling:       promhttp.ContinueOnError,
+			MaxRequestsInFlight: h.maxRequests,
+			Registry:            h.exporterMetricsRegistry,
+		},
+	)
+	if h.includeExporterMetrics {
+		// Note that we have to use h.exporterMetricsRegistry here to
+		// use the same promhttp metrics for all expositions.
+		handler = promhttp.InstrumentMetricHandler(
+			h.exporterMetricsRegistry, handler,
+		)
+	}
+	return handler, nil
 }
 
 func main() {
-	lg := log.NewJSONLogger(os.Stderr)
-	flag.Parse()
-	if printVersion {
-		if version == "" {
-			info, ok := debug.ReadBuildInfo()
-			if ok {
-				version = info.Main.Version
-			} else {
-				version = "devel"
-			}
-		}
-		if commit == "" {
-			commit = "unknown"
-		}
-		if date == "" {
-			date = "unknown"
-		}
-		b, err := json.Marshal(struct {
-			Version string `json:"version"`
-			Commit  string `json:"commit"`
-			Date    string `json:"date"`
-		}{
-			Version: version,
-			Commit:  commit,
-			Date:    date,
-		})
-		if err != nil {
-			level.Error(lg).Log("err", err)
-			os.Exit(1)
-		}
-		fmt.Println(string(b))
-		os.Exit(0)
-	}
-
-	reg := prometheus.NewRegistry()
-	reg.MustRegister(
-		prometheus.NewBuildInfoCollector(),
-		prometheus.NewGoCollector(),
-		collector.New(collector.WithLogger(lg), collector.WithTimeout(timeout), collector.WithSoraURL(soraURL)),
+	// lg := log.NewJSONLogger(os.Stderr)
+	var (
+		listenAddress = kingpin.Flag(
+			"web.listen-address",
+			"Address on which to expose metrics and web interface.",
+		).Default(":9490").String()
+		metricsPath = kingpin.Flag(
+			"web.telemetry-path",
+			"Path under which to expose metrics.",
+		).Default("/metrics").String()
+		// sora_client や erlang_vm をフィルターで切るようにする
+		// disableExporterMetrics = kingpin.Flag(
+		// 	"web.disable-exporter-metrics",
+		// 	"Exclude metrics about the exporter itself (promhttp_*, process_*, go_*).",
+		// ).Bool()
+		// maxRequests = kingpin.Flag(
+		// 	"web.max-requests",
+		// 	"Maximum number of parallel scrape requests. Use 0 to disable.",
+		// ).Default("40").Int()
 	)
 
-	mux := http.NewServeMux()
-	mux.Handle(metricsPath, promhttp.InstrumentMetricHandler(reg, promhttp.HandlerFor(reg, promhttp.HandlerOpts{})))
+	promlogConfig := &promlog.Config{}
+	flag.AddFlags(kingpin.CommandLine, promlogConfig)
+	kingpin.Version(version.Print("sora_exporter"))
+	kingpin.CommandLine.UsageWriter(os.Stdout)
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+	logger := promlog.New(promlogConfig)
 
-	srv := &http.Server{
-		Addr:    listenAddr,
-		Handler: mux,
+	level.Info(logger).Log("msg", "Starting sora_exporter", "version", version.Info())
+	level.Info(logger).Log("msg", "Build context", "build_context", version.BuildContext())
+
+	// 外だししたほうがいい
+	// reg := prometheus.NewRegistry()
+	// reg.MustRegister(
+	// 	prometheus.NewBuildInfoCollector(),
+	// 	prometheus.NewGoCollector(),
+	// 	collector.New(collector.WithLogger(lg), collector.WithTimeout(timeout), collector.WithSoraURL(soraURL)),
+	// )
+
+	// 外だししたほうがいい
+	// mux := http.NewServeMux()
+	// mux.Handle(metricsPath, promhttp.InstrumentMetricHandler(reg, promhttp.HandlerFor(reg, promhttp.HandlerOpts{})))
+
+	// root 権限で起動してたら warning を出す
+	if user, err := user.Current(); err == nil && user.Uid == "0" {
+		level.Warn(logger).Log("msg", "Sora Exporter is running as root user. This exporter is designed to run as unpriviledged user, root is not required.")
 	}
 
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer cancel()
-	finish := make(chan struct{})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+			<head><title>Sora Exporter</title></head>
+			<body>
+			<h1>Sora Exporter</h1>
+			<p><a href="` + *metricsPath + `">Metrics</a></p>
+			</body>
+			</html>`))
+	})
 
-	go func() {
-		defer close(finish)
-		<-ctx.Done()
-		shutDownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		if err := srv.Shutdown(shutDownCtx); err != nil {
-			if errors.Is(err, http.ErrServerClosed) {
-				return
-			}
-			level.Error(lg).Log("msg", "failed to shutdown", "err", err)
-		}
-	}()
-
-	if err := srv.ListenAndServe(); err != nil {
-		if !errors.Is(err, http.ErrServerClosed) {
-			level.Error(lg).Log("msg", "failed to listen", "err", err)
-		}
+	level.Info(logger).Log("msg", "Listening on", "address", *listenAddress)
+	server := &http.Server{Addr: *listenAddress}
+	if err := web.ListenAndServe(server, "", logger); err != nil {
+		level.Error(logger).Log("err", err)
 		os.Exit(1)
 	}
-	<-finish
-	os.Exit(0)
 }
