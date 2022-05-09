b04ea6d8a00ddb916c6316b97866f3f837e5c41f
Takeshi NAMAO <namao.takeshi@gmail.com>
2022-05-09 11:53:10 +0900

    メトリクス名の接頭辞を sora_exporter から sora に変更する

--- collector/collector.go
+++ collector/collector.go
@@ -126,11 +126,11 @@ func (c *Collector) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func newDesc(name, help string) *prometheus.Desc {
-	return prometheus.NewDesc(prometheus.BuildFQName("sora", "exporter", name), help, nil, nil)
+	return prometheus.NewDesc(prometheus.BuildFQName("sora", "", name), help, nil, nil)
 }
 
 func newDescWithLabel(name, help string, labels []string) *prometheus.Desc {
-	return prometheus.NewDesc(prometheus.BuildFQName("sora", "exporter", name), help, labels, nil)
+	return prometheus.NewDesc(prometheus.BuildFQName("sora", "", name), help, labels, nil)
 }
 
 func newGauge(d *prometheus.Desc, v float64) prometheus.Metric {
