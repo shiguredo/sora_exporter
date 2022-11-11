8bdf1044eab258f601178e7700c02a7f889964c7
Takeru Ohta <phjgt308@gmail.com>
2022-11-11 16:47:35 +0900

    Raft 関連のメトリクスを追加

--- collector/cluster_node.go
+++ collector/cluster_node.go
@@ -1,3 +1,4 @@
+// TODO: rename file
 package collector
 
 import "github.com/prometheus/client_golang/prometheus"
@@ -5,18 +6,27 @@ import "github.com/prometheus/client_golang/prometheus"
 var (
 	soraClusterMetrics = SoraClusterMetrics{
 		clusterNode: newDescWithLabel("cluster_node", "The sora server known cluster node.", []string{"node_name", "mode"}),
+		raftRole:        newDescWithLabel("cluster_raft_role", "The current Raft role (follower or leader). The role name is indicated by the label 'role'. The value of this metric is always set to 1.", []string{"role"}),
+		raftTerm:        newDesc("cluster_raft_term", "The current Raft term."),
+		raftCommitIndex: newDesc("cluster_raft_commit_index", "The latest committed Raft log index."),
 	}
 )
 
 type SoraClusterMetrics struct {
-	clusterNode *prometheus.Desc
+	clusterNode     *prometheus.Desc
+	raftRole        *prometheus.Desc
+	raftTerm        *prometheus.Desc
+	raftCommitIndex *prometheus.Desc
 }
 
 func (m *SoraClusterMetrics) Describe(ch chan<- *prometheus.Desc) {
 	ch <- m.clusterNode
+	ch <- m.raftRole
+	ch <- m.raftTerm
+	ch <- m.raftCommitIndex
 }
 
-func (m *SoraClusterMetrics) Collect(ch chan<- prometheus.Metric, nodeList []soraClusterNode) {
+func (m *SoraClusterMetrics) Collect(ch chan<- prometheus.Metric, nodeList []soraClusterNode, report soraClusterReport) {
 	for _, node := range nodeList {
 		if node.ClusterNodeName != nil {
 			ch <- newGauge(m.clusterNode, 1, *node.ClusterNodeName, *node.Mode)
@@ -24,4 +34,7 @@ func (m *SoraClusterMetrics) Collect(ch chan<- prometheus.Metric, nodeList []sor
 			ch <- newGauge(m.clusterNode, 1, *node.NodeName, *node.Mode)
 		}
 	}
+	ch <- newGauge(m.raftRole, 1.0, report.RaftRole)
+	ch <- newCounter(m.raftTerm, float64(report.RaftTerm))
+	ch <- newCounter(m.raftCommitIndex, float64(report.RaftCommitIndex))
 }
